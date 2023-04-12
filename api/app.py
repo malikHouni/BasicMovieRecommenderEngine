d9017568f1f9fa2b6ddae34e987e51476649ad95
malik houni <malik.houni@gmail.com>
2023-04-12 15:33:34 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -61,7 +61,7 @@ app = Flask(__name__, template_folder='./')
   
 @app.route("/")
 def home():
-    return render_template('index.html',data=getAllMovies())
+    return render_template('index.html',data=["toto1","toto2"])
 
 @app.route("/routeListOfMoviesLiked" ,methods=["GET", "POST"])
 def getMyRecommendation():
