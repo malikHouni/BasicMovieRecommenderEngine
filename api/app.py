961604c46f51b60582b8753070268ed3ce40df33
malik houni <malik.houni@gmail.com>
2023-04-12 16:32:26 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from scipy.sparse.linalg import svds
 
 from flask import Flask, render_template
 
@@ -8,4 +7,4 @@ app = Flask(__name__, template_folder='./')
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html',data=['toto','tata',"lala","lili","added scipy"])
+    return render_template('index.html',data=['toto','tata',"lala","lili","removed scipy"])
