403c1f7be6e3271a21ecd920dbe9778278c71894
malik houni <malik.houni@gmail.com>
2023-04-12 15:55:57 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,11 @@
+import numpy as np
+import pandas as pd
+from scipy.sparse.linalg import svds
+
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='./')
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html',data=['toto','tata'])
+    return render_template('index.html',data=['toto','tata',"lala"])
